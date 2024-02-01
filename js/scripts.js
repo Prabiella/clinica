472214d2d3754a03b4ef48eb4321e8474c185a6d
Francisco Rabiella <paccote@hotmail.com>
2024-02-01 08:33:08 -0600

    ajustes

--- js/scripts.js
+++ js/scripts.js
@@ -18,36 +18,12 @@ function togglePasswordVisibility() {
 
 
 
-/* Validación del formulario */
-  function validarFormulario() {
-    // Obtén todos los elementos de entrada del formulario
-    var inputs = document.querySelectorAll('input');
-    
-    // Itera sobre cada elemento de entrada
-    inputs.forEach(function(input) {
-      var feedbackElement = input.nextElementSibling; // Suponiendo que el elemento de retroalimentación sigue inmediatamente al input
-  
-      // Verifica si el campo está vacío
-      if (input.value.trim() === '') {
-        // Si está vacío, agrega las clases correspondientes
-        input.classList.add('is-invalid');
-        feedbackElement.classList.remove('d-none');
-        feedbackElement.textContent = 'Ingresa un dato válido';
-      } else {
-        // Si no está vacío, quita las clases correspondientes y ajusta el texto
-        input.classList.remove('is-invalid');
-        input.classList.add('is-valid');
-        feedbackElement.classList.add('valid-feedback');
-        feedbackElement.textContent = 'Dato válido';
-      }
-    });
-  }
-  
+
 
   /* Script para el switch */
   function cambiarTexto() {
     var miSwitch = document.getElementById("okSwitch");
-    var label = document.querySelector('label[for="okSwitch"]');
+    var label = document.querySelector('span[for="okSwitch"]');
 
     // Cambia el texto del label basado en el estado del switch
     label.textContent = miSwitch.checked ? "Sí" : "No";
@@ -55,33 +31,75 @@ function togglePasswordVisibility() {
 
 
 
-/* Canva */
+/* Canvas */
+
+
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var painting = false;
+
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", stopPainting);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("touchstart", startPainting);
+canvas.addEventListener("touchend", stopPainting);
+canvas.addEventListener("touchmove", draw);
+
+function startPainting(e) {
+    painting = true;
+    draw(e);
+}
+
+function stopPainting() {
+    painting = false;
+    context.beginPath();
+}
+
+function draw(e) {
+    e.preventDefault(); // Evita el comportamiento táctil predeterminado (por ejemplo, hacer zoom en la página)
+
+    var x, y;
+
+    if (e.touches) {
+        x = e.touches[0].clientX - canvas.offsetLeft;
+        y = e.touches[0].clientY - canvas.offsetTop;
+    } else {
+        x = e.clientX - canvas.offsetLeft;
+        y = e.clientY - canvas.offsetTop;
+    }
+
+    if (!painting) return;
+
+    context.lineWidth = 2;
+    context.lineCap = "round";
+    context.strokeStyle = "black";
+
+    context.lineTo(x, y);
+    context.stroke();
+    context.beginPath();
+    context.moveTo(x, y);
+}
 
 function mostrarImagen() {
-  var lista = document.getElementById("lista");
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext("2d");
+    var lista = document.getElementById("lista");
 
-  // Rutas de las imágenes según la opción seleccionada
-  var imagenes = {
-      Cabeza_cuello: "/img/canva/cabeza_cuello.svg",
-      Tronco: "/img/canva/tronco.svg",
-      Rodillas: "/img/canva/rodillas.svg",
-      Miembros_superiores: "/img/canva/miembros_superiores.svg",
-      Miembros_inferiores: "/img/canva/miembros_inferiores.svg",
-  };
+    // Rutas de las imágenes según la opción seleccionada
+    var imagenes = {
+        Cabeza_cuello: "img/canva/cabeza_cuello.svg",
+        Tronco: "img/canva/tronco.svg",
+        Rodillas: "img/canva/rodillas.svg",
+        Miembros_superiores: "img/canva/miembros_superiores.svg",
+        Miembros_inferiores: "img/canva/miembros_inferiores.svg",
+    };
 
-  // Limpia el canvas
-  context.clearRect(0, 0, canvas.width, canvas.height);
+    // Limpia el canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Carga la imagen seleccionada en el canvas
-  var imagenSeleccionada = new Image();
-  imagenSeleccionada.src = imagenes[lista.value];
+    // Carga la imagen seleccionada en el canvas
+    var imagenSeleccionada = new Image();
+    imagenSeleccionada.src = imagenes[lista.value];
 
-  imagenSeleccionada.onload = function () {
-      context.drawImage(imagenSeleccionada, 0, 0, canvas.width, canvas.height);
-  };
-
-
-  
+    imagenSeleccionada.onload = function () {
+        context.drawImage(imagenSeleccionada, 0, 0, canvas.width, canvas.height);
+    };
 }
